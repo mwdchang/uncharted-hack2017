21b8613b8d627b461989ae1df91eb2793620306c
William Gelhar <wgelhar@uncharted.software>
2017-08-16 10:54:27 -0400

    listening to websocket, logging to console

--- ui/src/index.js
+++ ui/src/index.js
@@ -12,3 +12,16 @@ if (!searchResultsContainer) {
 websocketData
     .map(searchResult => new SearchResultComponent(searchResult))
     .forEach(component => searchResultsContainer.appendChild(component.element));
+
+
+const searchesSocket = io('http://10.64.16.97:22222/'); //eslint-disable-line
+
+searchesSocket.on('connect', () =>{
+    console.log('connect')
+});
+searchesSocket.on('broadcast', (data) => {
+    console.log('event', data)
+});
+searchesSocket.on('disconnect', () => {
+    console.log('disconnect')
+});
