7bd96caa1a1930a2e95f08f76cf7ec14b4d8e0bb
William Gelhar <wgelhar@uncharted.software>
2017-08-16 13:13:13 -0400

    only displaying the last 20 search results

--- ui/src/index.js
+++ ui/src/index.js
@@ -7,7 +7,7 @@ if (!searchResultsContainer) {
     throw new Error('couldn\t find the searchResultsContainer element');
 }
 
-const browserCounts = new Map();
+let lastTwentySearchResultComponents = [];
 
 let listenToSocket = (socketUrl) => {
     const searchesSocket = io(socketUrl); //eslint-disable-line
@@ -18,11 +18,17 @@ let listenToSocket = (socketUrl) => {
     searchesSocket.on('broadcast', (searchResult) => {
         console.log('received searchResult', searchResult)
 
+
         const component = new SearchResultComponent(searchResult);
         searchResultsContainer.appendChild(component.element);
 
-        const countForBrowser = browserCounts.get(searchResult.browser) || 0;
-        browserCounts.set(searchResult.browser, countForBrowser + 1);
+        lastTwentySearchResultComponents.push(component.element);
+        lastTwentySearchResultComponents = lastTwentySearchResultComponents.slice(-20);
+
+        const childrenToRemove = [...searchResultsContainer.children].filter(child => lastTwentySearchResultComponents.indexOf(child) === -1);
+        childrenToRemove.forEach(child => {
+            searchResultsContainer.removeChild(child);
+        });
     });
     searchesSocket.on('disconnect', () => {
         console.log('disconnected from searches socket')
@@ -30,5 +36,3 @@ let listenToSocket = (socketUrl) => {
 };
 
 listenToSocket('http://10.64.16.97:22222/');
-
-const tryThingsOutSvg = d3.select("#trying-things-out").append("svg"); //eslint-disable-line
