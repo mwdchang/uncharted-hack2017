20ebad9727e5f1ed92fce467532db9ec62091255
William Gelhar <wgelhar@uncharted.software>
2017-08-16 10:57:01 -0400

    cleanup

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,27 +1,27 @@
 import './index.css';
 import SearchResultComponent from './components/search-result-component';
 
-import websocketData from './mock-data/websocket-data';
-
 const searchResultsContainer = document.getElementById('search-results-container');
 
 if (!searchResultsContainer) {
     throw new Error('couldn\t find the searchResultsContainer element');
 }
 
-websocketData
-    .map(searchResult => new SearchResultComponent(searchResult))
-    .forEach(component => searchResultsContainer.appendChild(component.element));
+let listenToSocket = (socketUrl) => {
+    const searchesSocket = io(socketUrl); //eslint-disable-line
 
+    searchesSocket.on('connect', () =>{
+        console.log('connect')
+    });
+    searchesSocket.on('broadcast', (searchResult) => {
+        console.log('event', searchResult)
 
-const searchesSocket = io('http://10.64.16.97:22222/'); //eslint-disable-line
+        const component = new SearchResultComponent(searchResult);
+        searchResultsContainer.appendChild(component.element);
+    });
+    searchesSocket.on('disconnect', () => {
+        console.log('disconnect')
+    });
+};
 
-searchesSocket.on('connect', () =>{
-    console.log('connect')
-});
-searchesSocket.on('broadcast', (data) => {
-    console.log('event', data)
-});
-searchesSocket.on('disconnect', () => {
-    console.log('disconnect')
-});
+listenToSocket('http://10.64.16.97:22222/');
